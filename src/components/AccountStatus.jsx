25c2f3c156afb572a2f3ed27f4fb41944f371ecf
Attah David <jocheattahdavid@gmail.com>
2021-03-19 11:40:33 +0100

    ui done

---
+++ src/components/AccountStatus.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AccountStatus() {
+  return <div>
+      <h1>Savings Acccount</h1>
+  </div>;
+}
+
+export default AccountStatus;
---
+++ src/components/Balance.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Balance() {
+  return <div>
+      <h1><span>$</span>100</h1>
+  </div>;
+}
+
+export default Balance;
